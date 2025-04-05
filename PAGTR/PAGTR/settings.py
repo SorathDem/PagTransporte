0d02102a145ba1be6d7e9d609d32150d1a2ba062
Sorath <santiagoacostaavila2905@gmail.com>
2025-04-04 21:45:08 -0500

    arreglo_static

--- PAGTR/PAGTR/settings.py
+++ PAGTR/PAGTR/settings.py
@@ -142,6 +142,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',  # (opcional, si tienes una carpeta 'static' con tus archivos personalizados)
+]
 
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
