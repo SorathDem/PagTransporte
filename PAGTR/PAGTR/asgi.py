69edc7ace60930dbe30a3f656369dc4bf03672b3
Sorath <santiagoacostaavila2905@gmail.com>
2025-04-07 21:40:02 -0500

    correcion_setings

--- PAGTR/PAGTR/asgi.py
+++ PAGTR/PAGTR/asgi.py
@@ -14,7 +14,7 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from TransporteAPP.routing import websocket_urlpatterns
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PAGTR.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PAGTR.PAGTR.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
